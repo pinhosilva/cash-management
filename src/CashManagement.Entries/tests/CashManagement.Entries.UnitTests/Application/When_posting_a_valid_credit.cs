8799c5a05da2c74fb91412e43c9b7378bbe9a12e
Rafael Pinho <rafaelpinho01@gmail.com>
2026-06-26 23:39:27 -0300

    test: uma classe de teste por arquivo (split de EntryTests e PostCreditCommandHandlerTests)

--- src/CashManagement.Entries/tests/CashManagement.Entries.UnitTests/Application/When_posting_a_valid_credit.cs
+++ src/CashManagement.Entries/tests/CashManagement.Entries.UnitTests/Application/When_posting_a_valid_credit.cs
@@ -3,7 +3,6 @@ using CashManagement.Entries.Application.Interfaces;
 using CashManagement.Entries.Domain.Aggregates;
 using CashManagement.Entries.Domain.Events;
 using CashManagement.Entries.Domain.Persistence;
-using CashManagement.Entries.Domain.SeedWork;
 using CashManagement.Entries.UnitTests.Fixtures;
 using Moq;
 using Shouldly;
@@ -33,28 +32,4 @@ public class When_posting_a_valid_credit
     [Fact]
     public void Then_the_aggregate_is_added_once() =>
         Repository.Verify(r => r.Add(It.IsAny<Entry>()), Times.Once);
-}
-
-public class When_posting_a_non_positive_credit
-    : CommandTestFixture<PostCreditCommand, PostCreditCommandHandler, Entry>
-{
-    protected override PostCreditCommand When() => new(0m, DateTime.UtcNow);
-
-    protected override PostCreditCommandHandler CreateHandler(IRepository repository, IIdGenerator idGenerator) =>
-        new(repository, idGenerator, new PostCreditCommandValidator());
-
-    [Fact]
-    public void Then_the_result_is_a_validation_failure()
-    {
-        Result.IsFailure.ShouldBeTrue();
-        Result.Error!.Type.ShouldBe(ErrorType.Validation);
-    }
-
-    [Fact]
-    public void Then_no_event_is_published() =>
-        PublishedEvents.ShouldBeEmpty();
-
-    [Fact]
-    public void Then_nothing_is_added() =>
-        Repository.Verify(r => r.Add(It.IsAny<AggregateRoot>()), Times.Never);
 }
